243e336da439a52e8fdadba13a5bd3bc68031742
Adam Ralph <adamralph@posteo.net>
2021-06-05 22:27:59 +1000

    added layout.html blueprint

--- test_app.py
+++ test_app.py
@@ -5,6 +5,7 @@ def test_index():
     tester = flaskblog.app.test_client()
     response = tester.get("/home", content_type="html/text")
 
-    #assert response.status_code == 200
-    #assert Path("templates/home.html")
+    assert response.status_code == 200
+    assert response.data == "posts"
+    assert Path("templates/home.html")
     
