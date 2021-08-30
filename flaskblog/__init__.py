000b8b0cb025964503e00f5639b433600e2eaf83
adamjralph <adamralph@posteo.net>
2021-08-30 11:31:34 +1000

    working user registration

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bycrypt
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '008c5fb26b126840a90252e5e98c8caa'
